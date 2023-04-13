e795c8263829c2d0d6f630fd813d258a5f3ab015
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-04-13 02:10:34 -0500

    added resources

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ import random
 from settings import *
 
 # Set the dimensions of the screen
-SCREEN_WIDTH = WIDTH
+SCREEN_WIDTH = WIDTH        #Located in settings
 SCREEN_HEIGHT = HEIGHT
 
+#game setup
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_title = pygame.display.set_caption("Snake")
+clock = pygame.time.Clock()
+running = True
 
+while running:
+    #check events and then enable quitting with the X button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    #Filling the screen with a color
+    screen.fill("black")
+
+    #RENDERING
+
+    #flip to put work on screen
+    pygame.display.flip()
+
+    clock.tick(60) #Limits fps to 60
+
+pygame.quit()
 
     
